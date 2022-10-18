0efad64ea62e738cc766723b98fc0a4862de0ea4
lamo47 <ljwells@ucsc.edu>
2022-10-18 14:43:22 -0700

    Messing around with JavaScript describing my car and Lab5

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/**
+  *Author: Landon Wells
+  *Created: 10.17.2022
+  *
+  * (c) Copyright by Landon Wells.
+**/
+
+// Define Variables
+make = "Nissan";
+model = "300zx";
+color = "Silver";
+year = "1987";
+
+// Calculate
+age = 2022 - year;
+
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "years<br>");
