ed1df3ad50be5b71248a15bc13f871b424c62fd7
lamo47 <ljwells@ucsc.edu>
2022-10-27 17:40:48 -0700

    creating a function that sorts the letters of a users name

---
+++ lab7/js/lab7.js
@@ -0,0 +1,25 @@
+/*
+* Author: Landon Wells <ljwells@ucsc.edu>
+* Created 22 September
+* License: Public Domain
+*/
+
+// sortUserName - function that takes users name and sorts the letters
+function sortUserName() {
+  var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
+  console.log("userName =", userName);
+  // split string to array
+  var nameArray = userName.split('');
+  console.log("nameArray =", nameArray);
+  // sort the array
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort =", nameArraySort);
+  // join array back to a string
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted =", nameSorted);
+  return nameSorted;
+}
+
+// output
+document.writeln("Oh hey, I've fixed your name: ",
+  sortUserName(), "</br>");
