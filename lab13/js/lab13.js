75bdebebc3daba616745b488076339fd3942377e
lamo47 <ljwells@ucsc.edu>
2022-11-15 19:23:47 -0800

    lab13 resubmit

--- lab13/js/lab13.js
+++ lab13/js/lab13.js
@@ -1,39 +1,66 @@
-for (var num=0; num <=200; num++) {
-  if (num%15 ==0) {
-    console.log("FizzBuzz");
+/*
+* Author: Landon Wells <ljwells@ucsc.edu>
+* Created 14 November
+* License: Public Domain
+*/
+
+maxFactors = 4;
+outputEl = document.getElementById("output");
+
+function getFactorObj() {
+  var factorObj = {};
+  for (var factor=0; factor<maxFactors; factor++) {
+    numId = "num" + factor;
+    textId = "text" + factor;
+    numValue = document.getElementById(numId).value;
+    textValue = document.getElementById(textId).value;
+    console.log(factor + ")num:", numValue, "text:", textValue)
+    if (numValue && textValue) {
+      factorObj[numValue] = textValue;
+    }
   }
-  else if (num%3 ==0) {
-    console.log("Fizz");
-  }
-  else if (num%4 ==0) {
-    console.log("Buzz");
-  }
-  else if (num%5 ==0) {
-    console.log("Boom");
-  }
-  else if (num%7 ==0) {
-    console.log("Bang");
-  }
-  else {
-    console.log(num);
-  }
+  return factorObj;
 }
+
+function outputToPage(str) {
+  newEl = document.createElement("p");
+  newEl.innerHTML = str;
+  outputEl.appendChild(newEl);
+}
+
 function fizzBuzzBoom(maxNums, factorObj) {
   for (var num=0; num<maxNums; num++) {
+    debugger;
     var outputStr = "";
     for (var factor in factorObj) {
-      if (num % factor ==0) {
+      if (num % factor == 0) {
         outputStr += factorObj[factor];
       }
     }
     if (outputStr) {
-      outputStr = " - " + outputStr + "!";
+      outputStr = "-" + outputStr + "!";
     }
     outputToPage(num.toString() + outputStr)
   }
 }
+function reportError(str) {
+  outputEl.innerHTML = "<div class= 'error'>" + str + "</div>";
+}
+
 document.getElementById("submit").addEventListener("click", function() {
-  var num = fizzBuzzBoom();
-  newText = num
-  document.getElementById("output").innerHTML = newText;
+  var max = document.getElementById("max").value;
+  console.log("max:", max)
+  if (! max) {
+    reportError("You must provide a maximum");
+    return;
+  }
+  var factorObj = getFactorObj();
+  console.log("factorObj:", factorObj);
+  if (Object.keys(factorObj).length == 0) {
+    reportError("You must provide at least one factor and text");
+    return;
+  }
+  outputEl.innerHTML = "";
+  fizzBuzzBoom(max, factorObj);
+  outputEl.classList.add("cols");
 })
